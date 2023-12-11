ae9e4ad8093724568ea533124b3ea0fe00de71a3
30784510@qq.com <260213qq>
2023-12-11 16:42:17 +0800

    修复脚手架Android安装目录

--- src/config.js
+++ src/config.js
@@ -1,7 +1,7 @@
 module.exports = {
   iOSRepo: 'https://github.com/fg2q1q3q/hera-ios-sample.git',
   androidRepo: 'https://github.com/fg2q1q3q/hera-android-sample.git',
-  androidAPKName: 'app/build/outputs/apk/app-debug.apk',
+  androidAPKName: 'app/build/outputs/apk/debug/app-debug.apk',
   androidManifestFile: 'app/src/main/AndroidManifest.xml',
   androidMainActivity: 'SampleActivity',
   tmpDistDir: 'heraTmp',
